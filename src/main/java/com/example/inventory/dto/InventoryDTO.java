d55bd954c3215cc4dff5dc3f82de342aa0c21cf4
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2025-04-03 20:57:56 +0530

    create inventory DTO

---
+++ src/main/java/com/example/inventory/dto/InventoryDTO.java
@@ -0,0 +1,18 @@
+package com.example.inventory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class InventoryDTO {
+
+    private int id;
+    private int itemId;
+    private int productId;
+    private int quantity;
+
+}
